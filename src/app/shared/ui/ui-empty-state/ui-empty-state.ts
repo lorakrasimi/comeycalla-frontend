9bbe74a9c7e159c8e056452709e24384782cb7bf
l.adova <l.adova@kamasys.de>
2026-04-25 22:06:38 +0200

    - empty-state

--- src/app/shared/ui/ui-empty-state/ui-empty-state.ts
+++ src/app/shared/ui/ui-empty-state/ui-empty-state.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {UiButton} from '../ui-button/ui-button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'ui-empty-state',
-  imports: [],
+  imports: [
+    UiButton,
+    RouterLink
+  ],
   templateUrl: './ui-empty-state.html',
   styleUrl: './ui-empty-state.scss',
 })
