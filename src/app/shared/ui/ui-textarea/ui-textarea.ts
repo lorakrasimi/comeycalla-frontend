9c1d36240cb7d7aaca8625290d6d1e0fe1564dee
l.adova <l.adova@kamasys.de>
2026-04-27 08:33:06 +0200

    - empty-state

--- src/app/shared/ui/ui-textarea/ui-textarea.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ui-textarea',
-  imports: [],
-  templateUrl: './ui-textarea.html',
-  styleUrl: './ui-textarea.scss',
-})
-export class UiTextarea {}
